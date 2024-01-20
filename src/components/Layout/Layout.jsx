74b10b31cc7c52e5a17dd338c2c49901eb8f2726
hoseingh7 <hoseingholamii1376@gmail.com>
2024-01-20 10:45:17 -0800

    adding the layout folder

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import Router from "../../routers/Router";
+const Layout = () => {
+  return (
+    <>
+      <Header />
+      <div>
+        <Router />
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
